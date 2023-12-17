807c53989ec69bc20ec8fbf6968823ad97b81132
Bmalley24 <bmalley24@kentdenver.org>
2023-12-17 11:21:31 -0600

    I have completeted my STACK QUEUE CODE. LETSSS GOOO!

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,8 +1,8 @@
 public class StackQueue {
 
     //Instance data
-    private Stack DValue; // dequeueing
-    private Stack EValue; // enqueueing
+    private Stack DValue; // first stack
+    private Stack EValue; // second stack
     private int size;
 
     //Empty Constructor
@@ -12,23 +12,47 @@ public class StackQueue {
         size = 0;
     }
 
-    //Methods
+    //METHODS
 
     //add an element
     public <E> void enqueue(E element) {
+        // Move all elements from DValue to EValue
+        while (!DValue.isEmpty()) //while DValue is not empty
+        {
+            EValue.push(DValue.pop());
+        }
 
+        // Push elements to DValue
+        DValue.push(element);
+
+        // Push everything back onto DValue
+        while (!EValue.isEmpty())//while EValue is not empty
+        {
+            DValue.push(EValue.pop());
+        }
+
+        size++;
     }
 
     //remove and return the least recent element
     public <E> E dequeue()  {
+        // if first stack is empty
+        if (DValue.isEmpty() == true ) {
+            return null;
+        }
 
+        // Return first input of Dvalue
+        E temp = (E) DValue.peek();
+        DValue.pop();
+
+        size--;
+
+        return temp;
     }
-
 
     public boolean isEmpty() {
         return size == 0;
     }
-
 
     public int size() {
         return size;
@@ -36,7 +60,7 @@ public class StackQueue {
 
     //look at the least recent element without removing
     public <E> E peek() {
-
+        return (E) DValue.peek();
     }
 
 }
